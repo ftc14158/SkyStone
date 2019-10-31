97c1e12a79368f34023ca42841b17a0a9c909aaf
ftc14158 <ftcwyan@schartz.net>
2019-10-31 09:11:15 -0400

    Mechanum and tracking classes and change main OpModes to use them

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanum.java
@@ -1,4 +1,151 @@
 package org.firstinspires.ftc.teamcode;
 
-public class Mechanum {
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.robocol.TelemetryMessage;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+import java.nio.channels.MulticastChannel;
+
+public class Mechanum  {
+
+
+    public DcMotor drive_fl;   // actual name on robot configuration is drive-fl
+    public DcMotor drive_fr;   // etc..
+    public DcMotor drive_rl;
+    public DcMotor drive_rr;
+
+    // Previous values for change detection
+    int PrevVD = 0; // Desired Robot Speed
+    int PrevThetaD = 0; // Desired Angle
+    int PrevVTheta = 0; // Desired Rotation Speed
+
+    public Mechanum(DcMotor fl, DcMotor fr, DcMotor rl, DcMotor rr) {
+        drive_fl = fl;
+        drive_fr = fr;
+        drive_rl = rl;
+        drive_rr = rr;
+    }
+
+    private void initializeDriveMotor(DcMotor m) {
+        m.setPower(0);
+        m.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        m.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    public void InitializeMotors() {
+        for(DcMotor m : new DcMotor[] {drive_fl, drive_fr, drive_rl, drive_rr} )
+            initializeDriveMotor(m);
+
+        // Previous values for change detection
+        int PrevVD = 0; // Desired Robot Speed
+        int PrevThetaD = 0; // Desired Angle
+        int PrevVTheta = 0; // Desired Rotation Speed
+    }
+
+    /**
+     **  Mechanum drive function
+     */
+    public void MechDrive(
+            int LR, // Left/Right Command
+            int FR, // Forward/Reverse Command
+            int CCW // Rotation command
+            ) {
+
+
+        int VD; // Desired Robot Speed
+        int ThetaD;  // Desired Angle
+        int VTheta;  // Desired Rotation Speed
+
+        int ThetaD45; // Desire Angle + 45o
+
+        int V1; // Front Left motor
+        int V2; // Front Right motor
+        int V3; // Rear Left motor
+        int V4; // Rear Right motor
+        int RadioVD; // VD from joystick
+        int RadioTh; // Theta from joystick
+
+        //Capture joystick value
+        // X and Y values need to be from -1000 to +1000
+        // standard values are from -1 to +1
+
+      //  LR = (int) ( gamepad1.left_stick_x * 1000 );
+      //  FR = (int) (-gamepad1.left_stick_y * 1000 );  // invert value because negative is up on joystick
+      //  CCW = (int) (gamepad1.right_stick_x * 1000 );
+
+        // Compute distance of joystick from center position in any direction
+        RadioVD = (int) ( ( Math.sqrt(LR * LR + FR * FR)) );
+
+        // Compute angle of X-Y
+        if( FR != 0 ) {
+            float x, y;
+            x = (float)LR;
+            y = (float)FR;
+
+            RadioTh = (int) ( Math.toDegrees( Math.atan( x/y ) ) ); // atan takes input * 1000 and returns angle in degrees * 10
+
+            //   telemetry.addData("Theta", "x "+x+" y "+y+" radioth "+RadioTh );
+
+            if( (LR >= 0) && (FR < 0) ) {
+                RadioTh += 180;
+            } else if( (LR < 0) && (FR < 0) ) {
+                RadioTh -= 180;
+            }
+        } else if (LR > 0) {
+            RadioTh = 90;
+        } else if (LR < 0) {
+            RadioTh = -90;
+        } else {
+            RadioTh = 0;
+        }
+
+        VD = RadioVD;
+        ThetaD = RadioTh;
+        VTheta = -CCW;
+
+        // Uncomment below to check captured values in console
+        //   telemetry.addData("Positions", "LR " + LR + " FR " + FR + " RadioVD " + RadioVD + " RadioTh " + RadioTh  );
+
+        // To avoid unnecessary computation, evaluate formulas only if change occurred
+
+        if( (VD != PrevVD) || (ThetaD != PrevThetaD) || (VTheta != PrevVTheta) ) {
+            ThetaD45 = ThetaD + 45; // compute once angle + 45 for use in the 4 equations
+
+            V1 = (int) ( (VD * Math.sin( Math.toRadians(ThetaD45) ) ) + VTheta ); // sin takes degrees and returns result * 1000
+            V2 = (int) ( (VD * Math.cos( Math.toRadians(ThetaD45) ) ) - VTheta );
+            V3 = (int) ( (VD * Math.cos( Math.toRadians(ThetaD45) ) ) + VTheta );
+            V4 = (int) ( (VD * Math.sin( Math.toRadians(ThetaD45) ) ) - VTheta );
+
+            // Uncomment below to view computed speeds in console
+
+            //telemetry.addData("Velocities", "V1 " +V1 + " V2 " + V2 + " V3 " + V3 + " V4 " + V4 );
+
+            // Save for detecting change at next loop execution
+
+            PrevVD = VD;
+            PrevThetaD = ThetaD;
+            PrevVTheta = VTheta;
+
+            // Apply to local motors
+            // v1 = fl, v2 = fr, v3 = rl, v4 = rr
+            float fl, fr, rl, rr;
+
+            fl = (float)V1 / 1000;
+            fr = (float)V2 / 1000;
+            rl = (float)V3 / 1000;
+            rr = (float)V4 / 1000;
+
+            drive_fl.setPower(fl);
+            drive_rl.setPower(rl);
+            drive_fr.setPower(-fr);
+            drive_rr.setPower(-rr);
+
+        } else { V1 = 0; V2 = 0; V3 = 0; V4 = 0; }
+
+
+    }
+
+
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tracking.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tracking.java
@@ -4,26 +4,34 @@ import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 
 import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.firstinspires.ftc.robotcore.external.navigation.Position;
 import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
 
-public class IMU {
+public class Tracking {
 
-    // The IMU sensor object
+    // The Tracking sensor object
     public BNO055IMU imu;
 
     // State used for updating telemetry
     Orientation angles;
     Acceleration gravity;
+    Position curPosition;
+
+    public Tracking( BNO055IMU imuInterface ) {
+        imu = imuInterface;
+    }
 
     /***
-     * Initialize the IMU with calib data file if available
+     * Initialize the Tracking with calib data file if available
      *
      * @return true if succeeds
      */
     public boolean initialize() {
-        // Set up the parameters with which we will use our IMU. Note that integration
+        // Set up the parameters with which we will use our Tracking. Note that integration
         // algorithm here just reports accelerations to the logcat log; it doesn't actually
         // provide positional information.
         BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
@@ -34,10 +42,10 @@ public class IMU {
         parameters.loggingTag          = "IMU";
         parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
 
-        // Retrieve and initialize the IMU. We expect the IMU to be attached to an I2C port
-        // on a Core Device Interface Module, configured to be a sensor of type "AdaFruit IMU",
+        // Retrieve and initialize the Tracking. We expect the Tracking to be attached to an I2C port
+        // on a Core Device Interface Module, configured to be a sensor of type "AdaFruit Tracking",
         // and named "imu".
-        imu = hardwareMap.get(BNO055IMU.class, "imu");
+
         return imu.initialize(parameters);
 
     }
@@ -51,7 +59,26 @@ public class IMU {
         imu.startAccelerationIntegration(new Position(), new Velocity(), 1000);
     }
 
-    public double heading() {
-
+    /***
+     * Call this regularly within loop to get updated tracking information
+     */
+    public void readTracking() {
+        // Acquiring the angles is relatively expensive; we don't want
+        // to do that in each of the three items that need that info, as that's
+        // three times the necessary expense.
+        angles   = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+        // gravity  = imu.getGravity();
+        curPosition = imu.getPosition();
     }
+
+    public float heading() {
+        return angles.firstAngle;
+    }
+
+    public Position position() {
+        return curPosition;
+    }
+
+
+
 }
